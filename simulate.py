4af7ef428e416ed70067a69eedc6771de3db4fb5
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-06-10 22:36:26 -0400

    Enabled Physics World Simulation View.

--- simulate.py
+++ simulate.py
@@ -1,5 +1,13 @@
+from time import time
+import time
 import pybullet as p
 
 passphysicsClient = p.connect(p.GUI)
+
+for i in range(1000):
+    p.stepSimulation()
+    print(i)
+    time.sleep(0.01)
+
 
 p.disconnect()
