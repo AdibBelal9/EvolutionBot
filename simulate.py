6ac134a630c1965035654c0b48361455fb3aefb8
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-07-07 01:58:46 -0400

    Added floor and gravity

--- simulate.py
+++ simulate.py
@@ -1,16 +1,21 @@
 from time import time
 import time
 import pybullet as p
+import pybullet_data
 
 passphysicsClient = p.connect(p.GUI)
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("box.sdf")
 
 for i in range(1000):
     p.stepSimulation()
     print(i)
-    time.sleep(0.01)
-
+    time.sleep(0.001)
 
 p.disconnect()
 # testing 
