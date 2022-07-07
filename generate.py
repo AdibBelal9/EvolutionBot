de78de0ac242d1dabfa1cb8aa09083fb5617a6d5
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-07-07 02:49:14 -0400

    Link at origin and commented a for loop (inactive)

--- generate.py
+++ generate.py
@@ -1,6 +1,28 @@
+from ctypes import sizeof
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxes.sdf")
+
+# Box dimensions
+length = 1
+width = 1
+height = 1
+# Box position
+x = 0
+y = 0
+z = 0.5
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+
 
 pyrosim.End()
+
+
+
+
+
+# for loops for multiple links:
+    #for s in range(10):
+    #   pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+    #    if z == z:
+    #        z = z + 1
+    #    print(s)
