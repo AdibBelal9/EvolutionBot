6d7700d8bb104e9055b89e99932d9779400f490b
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-07-07 02:50:25 -0400

    Updated simulate.py file accordingly

--- simulate.py
+++ simulate.py
@@ -10,12 +10,12 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 
-p.loadSDF("box.sdf")
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
     print(i)
-    time.sleep(0.001)
-
+    time.sleep(0.05)
+    
 p.disconnect()
 # testing 
