350ce3fbc1206c0c8e48a6b16b1ec382dbe2ea06
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-07-08 22:32:41 -0400

    Added robotIF and body.urdf line along with world.sdf

--- simulate.py
+++ simulate.py
@@ -3,19 +3,23 @@ import time
 import pybullet as p
 import pybullet_data
 
+
+
 passphysicsClient = p.connect(p.GUI)
 
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
-p.loadSDF("boxes.sdf")
+
+p.loadSDF("world.sdf")
 
 for i in range(1000):
     p.stepSimulation()
     print(i)
     time.sleep(0.05)
-    
+
 p.disconnect()
 # testing 
