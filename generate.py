a585f38208caf1677c6d1f1449ef3c3e2dcde6d7
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-07-13 00:31:57 -0400

    added join Link0_Link1

--- generate.py
+++ generate.py
@@ -26,9 +26,9 @@ def Create_Robot():
     x = 0
     y = 0
     z = 0.5
-    pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length,width,height])
-    pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5,0,1])
-    pyrosim.Send_Cube(name="Leg", pos=[1,0,1.5] , size=[1,1,1])
+    pyrosim.Send_Cube(name="Link0", pos=[x,y,z] , size=[length,width,height])
+    pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5] , size=[1,1,1])
     pyrosim.End()
 
 Create_Robot()
