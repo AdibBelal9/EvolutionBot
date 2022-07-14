859a40bf3ba599e9220cf998850155eadcd7e5ff
Adib Belal <adibbelal@Adibs-M1-MBP.lan>
2022-07-14 17:45:33 -0400

    graphed frontLegSensorValues from stored values and customized graph

--- analyze.py
+++ analyze.py
@@ -1,11 +1,16 @@
+from turtle import color
 import matplotlib.pyplot
 import numpy 
 
-b = numpy.load("data/Store.npy", mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII')
-a = print(numpy.load("data/Store.npy", mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII'))
+b = numpy.load("data/backLegSensorValues.npy", mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII')
+a = print(numpy.load("data/backLegSensorValues.npy", mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII'))
 
-matplotlib.pyplot.plot(b)
-# , **kwargs
+d = numpy.load("data/frontLegSensorValues.npy", mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII')
+c = print(numpy.load("data/frontLegSensorValues.npy", mmap_mode=None, allow_pickle=True, fix_imports=True, encoding='ASCII'))
 
+matplotlib.pyplot.plot(b, linewidth=3, color='green')
+matplotlib.pyplot.plot(d, color='purple')
+
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
 
